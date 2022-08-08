24e98435ca9a2e9ed71b714b654c522ecf1de13e
cisco <sovisrushain@gmail.com>
2022-08-09 02:36:20 +0530

    01 var

--- 01-Var-KeyWord/src/demo01/Main.java
+++ 01-Var-KeyWord/src/demo01/Main.java
@@ -1,8 +1,16 @@
 package demo01;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        var age = 12;
+        var greet = "hello world";
+        var list = new ArrayList<Integer>(List.of(1,2,3,4,5));
+        System.out.println(age);
+        System.out.println(greet);
+        list.forEach(System.out::println);
     }
 }
