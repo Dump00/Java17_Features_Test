af6d6bcd48f24e0f19c2c3a0fa5638d6084bfdb7
cisco <sovisrushain@gmail.com>
2022-08-11 02:23:55 +0530

    05 optionals

---
+++ Optional/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        Optional<Cat> tutu = findCatByName("Tutu");
+
+        Integer age = tutu.map(Cat::getAge).orElse(0);
+        System.out.println(age);
+
+//        Cat defCat = tutu.orElse(new Cat("UNKNOWN", 0));  
+//        System.out.println(defCat.getAge());
+
+//        if(tutu.isPresent()) {
+//            System.out.println(tutu.get().getAge());
+//        } else {
+//            System.out.println(0);
+//        }
+
+//        if(tutu != null) {
+//            System.out.println(tutu.getAge());
+//        }else {
+//            System.out.println(0);
+//        }
+    }
+
+    public static Optional<Cat> findCatByName(String name) {
+        //var cat = new Cat(name, 3);
+        Cat cat = null;
+        return Optional.ofNullable(cat);
+    }
+}
+
